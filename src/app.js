16786425514e9501eea7fcc3577e1908bffc7876
scorusadrian14 <dev.adrianscorus14@gmail.com>
2023-12-28 11:44:46 +0200

    setup basic express app

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
+const middlewares = require("./middlewares");
 
 const app = express();
 app.use(morgan("tiny"));
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
     message: "🏠📦🥫 Welcome to Home inventory application 🏠📦🥫",
   });
 });
+
+//ERROR HANDLING
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 module.exports = app;
